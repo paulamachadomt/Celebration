99032771f7aad33c365d50ab1f197e8f215416d6
Paula <paula@matrix>
2020-06-09 16:12:37 -0300

    mudança2 na estrutura do projeto

--- projetofinaldb/src/main/Java/model/Pessoa.java
+++ projetofinaldb/src/main/Java/model/Pessoa.java
@@ -4,5 +4,14 @@ import lombok.Data;
 
 @Data
 public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String escolhaItem;
+
+    public Pessoa (String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
     
 }
