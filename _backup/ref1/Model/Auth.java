cd9f74c3c015e5bcf705c0f9e04ef8d5dc42528b
rafael <matrix@rafael>
2020-06-09 21:14:24 -0300

    rm backup

--- _backup/ref1/Model/Auth.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class Auth {
-    public static boolean isAllowed(String username, String password) {
-        return username.contentEquals("admin") && password.contentEquals("password");
-    }
-}
