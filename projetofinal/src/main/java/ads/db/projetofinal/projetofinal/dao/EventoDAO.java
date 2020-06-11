5080d6e5b65f1cd7c39f2275f309217e46a8e8dd
Paula <paula@matrix>
2020-06-11 11:36:01 -0300

    crud comesbebes e evento

--- projetofinal/src/main/java/ads/db/projetofinal/projetofinal/dao/EventoDAO.java
+++ projetofinal/src/main/java/ads/db/projetofinal/projetofinal/dao/EventoDAO.java
@@ -1,61 +1,124 @@
-// package ads.db.projetofinal.projetofinal.dao;
+package ads.db.projetofinal.projetofinal.dao;
 
-// import java.sql.Connection;
-// import java.sql.PreparedStatement;
-// import java.sql.ResultSet;
-// import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
-// import ads.db.projetofinal.projetofinal.dao.jdbc.Conexao;
-// import ads.db.projetofinal.projetofinal.model.Evento;
-// import ads.db.projetofinal.projetofinal.model.Pessoa;
+import ads.db.projetofinal.projetofinal.dao.jdbc.Conexao;
+import ads.db.projetofinal.projetofinal.model.Evento;
+import ads.db.projetofinal.projetofinal.model.Pessoa;
 
-// public class EventoDAO {
+public class EventoDAO {
 
-//     public boolean cadastroEvento(Evento evento){
-//         boolean resultado = false;
-//         try {
-//             Connection conexao = Conexao.getConexao();
-//             String comandoSQL = "INSERT INTO evento VALUES (?, ?, ?, ?)";
-//             PreparedStatement statement = conexao.prepareStatement(comandoSQL);
-//             statement.setInt(1, evento.getSenha());
-//             statement.setString(2, evento.getLocal());
-//             statement.setDate(3, evento.getData());
-//             statement.setString(4, evento.getDescricao());
-//             if (statement.executeUpdate() >= 1) {
-//                 resultado = true;
-//             }
-//             statement.close();
-//             conexao.close();
-//         } catch (Exception e) {
-//             System.out.println("Erro ao cadastrar evento \n" + e);
-//         }
-//         return resultado;
-//     }
+    public boolean cadastroEvento(Evento evento) {
+        boolean resultado = false;
+        try {
+            Connection conexao = Conexao.getConexao();
+            String comandoSQL = "INSERT INTO evento (senha, local, data, descricao, nome) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = conexao.prepareStatement(comandoSQL);
+            statement.setInt(1, evento.getSenha());
+            statement.setString(2, evento.getLocal());
+            statement.setDate(3, Date.valueOf(evento.getData()));
+            statement.setString(4, evento.getDescricao());
+            statement.setString(5, evento.getNome());
+            if (statement.executeUpdate() >= 1) {
+                resultado = true;
+            }
+            statement.close();
+            conexao.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar evento \n" + e);
+        }
+        return resultado;
+    }
 
-//     public Evento selectCodEvento(Integer codigo){
-//         Evento evento = null;
-//         try {
-//             Connection conexao = Conexao.getConexao();
-//             String comandoSQL = "SELECT * FROM evento WHERE codigo = ?";
-//             PreparedStatement statement = conexao.prepareStatement(comandoSQL);
-//             statement.setInt(1, codigo);
-//             ResultSet resultadoSelect = statement.executeQuery();
-//             while (resultadoSelect.next()) {
-//                 evento = new EventoDAO(resultadoSelect.getInt("codigo"), resultadoSelect.getString("nome"));
-//             }
-//             resultadoSelect.close();
-//             statement.close();
-//             conexao.close();
-//         } catch (Exception e) {
-//             System.out.println("Erro ao localizar pessoa por CPF \n" + e);
-//         }
-//         return evento;
+    public Evento selectSenhaEvento(Integer senha) {
+        Evento evento = null;
+        try {
+            Connection conexao = Conexao.getConexao();
+            String comandoSQL = "SELECT * FROM evento WHERE senha = ?";
+            PreparedStatement statement = conexao.prepareStatement(comandoSQL);
+            statement.setInt(1, senha);
+            ResultSet resultadoSelect = statement.executeQuery();
+            while (resultadoSelect.next()) {
+                evento = new Evento(resultadoSelect.getString("local"), resultadoSelect.getDate("data").toLocalDate(),
+                        resultadoSelect.getString("descricao"), resultadoSelect.getString("nome"));
+                evento.setCodigo(resultadoSelect.getInt("codigo"));
+            }
+            resultadoSelect.close();
+            statement.close();
+            conexao.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao localizar evento por senha \n" + e);
+        }
+        return evento;
+    }
 
-//     }
+    public Evento selectCodEvento(Integer codigo) {
+        Evento evento = null;
+        try {
+            Connection conexao = Conexao.getConexao();
+            String comandoSQL = "SELECT * FROM evento WHERE senha = ?";
+            PreparedStatement statement = conexao.prepareStatement(comandoSQL);
+            statement.setInt(1, codigo);
+            ResultSet resultadoSelect = statement.executeQuery();
+            while (resultadoSelect.next()) {
+                evento = new Evento(
+                    resultadoSelect.getString("local"), 
+                    resultadoSelect.getDate("data").toLocalDate(),
+                    resultadoSelect.getString("descricao"), 
+                    resultadoSelect.getString("nome"));
+                evento.setCodigo(resultadoSelect.getInt("codigo"));
+            }
+            resultadoSelect.close();
+            statement.close();
+            conexao.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao localizar evento por codigo \n" + e);
+        }
+        return evento;
+    }
+ 
+    public Boolean updateEvento(Evento evento) {
+        boolean resultado = false;
+        try {
+            Connection conexao = Conexao.getConexao();
+            String comandoSQL = "UPDATE evento SET senha = ?, local = ?, data = ?, descricao = ?, nome = ? WHERE codigo = ?";
+            PreparedStatement statement = conexao.prepareStatement(comandoSQL);
+            statement.setInt(1, evento.getSenha());
+            statement.setString(2, evento.getLocal());
+            statement.setDate(3, Date.valueOf(evento.getData()));
+            statement.setString(4, evento.getDescricao());
+            statement.setString(5, evento.getNome());
+            if (statement.executeUpdate() >= 1) {
+                resultado = true;
+            }
+            statement.close();
+            conexao.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao localizar evento " + e);
+        }
+        return resultado;
+    }
 
+    public Boolean deleteEvento(Integer codigo) {
+        boolean resultado = false;
+        try {
+            Connection conexao = Conexao.getConexao();
+            String comandoSQL = "DELETE FROM evento WHERE codigo = ?";
+            PreparedStatement statement = conexao.prepareStatement(comandoSQL);
+            statement.setInt(1, codigo);
+            if (statement.executeUpdate() >= 1) {
+                resultado = true;
+            }
+            statement.close();
+            conexao.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao localizar evento " + e);
+        }
+        return resultado;
+    }
 
-
-
-
-    
-// }
+}
--- projetofinal/src/main/java/ads/db/projetofinal/projetofinal/model/Evento.java
+++ projetofinal/src/main/java/ads/db/projetofinal/projetofinal/model/Evento.java
@@ -13,14 +13,14 @@ public class Evento {
     private String local;
     private LocalDate data;
     private String descricao;
+    private String nome;
     private ArrayList<Pessoa> convidados;
     private ArrayList<ComesBebes> comesBebes;
     
-    public Evento(String local, LocalDate data, String descricao) {
+    public Evento(String local, LocalDate data, String descricao, String nome) {
         this.local = local;
         this.data = data;
         this.descricao = descricao;
-        
+        this.nome = nome;
     }
-    
 }
