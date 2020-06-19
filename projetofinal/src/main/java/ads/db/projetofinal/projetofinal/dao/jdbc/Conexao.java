33a1b86dcfdef25550067f4c656e910a961b8b2f
rafael <matrix@rafael>
2020-06-19 17:29:59 -0300

    docker link

--- projetofinal/src/main/java/ads/db/projetofinal/projetofinal/dao/jdbc/Conexao.java
+++ projetofinal/src/main/java/ads/db/projetofinal/projetofinal/dao/jdbc/Conexao.java
@@ -4,18 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Conexao {
-    
+
     public static Connection getConexao() {
         Settings settings = new Settings();
         Connection conexao = null;
-        try { Class.forName(settings.getDriver());
-            try { conexao = DriverManager.getConnection(
-                    settings.getUrl(),
-                    settings.getUser(),
-                    settings.getSenha()
-                );
-            } catch (Exception e) { System.out.println("erro ao conectar no banco \n" + e); }
-        } catch (Exception e) { System.out.println("erro ao pesquisar driver \n" + e); }
+        try {
+            Class.forName(settings.getDriver());
+            try {
+                try {
+                    conexao = DriverManager.getConnection(settings.getUrl_localhost(), settings.getUser(), settings.getSenha());
+                } catch (Exception e) {
+                    conexao = DriverManager.getConnection(settings.getUrl_docker(), settings.getUser(), settings.getSenha());
+                }
+            } catch (Exception e) {
+                System.out.println("erro ao conectar no banco \n" + e);
+            }
+        } catch (Exception e) {
+            System.out.println("erro ao pesquisar driver \n" + e);
+        }
         return conexao;
     }
 }
--- projetofinal/src/main/java/ads/db/projetofinal/projetofinal/dao/jdbc/Settings.java
+++ projetofinal/src/main/java/ads/db/projetofinal/projetofinal/dao/jdbc/Settings.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class Settings {
     private String driver = "com.mysql.cj.jdbc.Driver";
-    private String url = "jdbc:mysql://localhost:3306/projetofinaldb?useTimezone=true&serverTimezone=UTC";
+    private String url_localhost = "jdbc:mysql://localhost:3306/projetofinaldb?useTimezone=true&serverTimezone=UTC";
+    private String url_docker = "jdbc:mysql://172.17.0.1:3306/projetofinaldb?useTimezone=true&serverTimezone=UTC";
     private String user = "root";
     private String senha = "123456";
 }
